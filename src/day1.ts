00c4f306232d36b2fb455987974bc5d4dadb2e51
mrhart1ey <32621099+mrhart1ey@users.noreply.github.com>
2022-12-03 12:17:47 +0000

    Updated linting

--- src/day1.ts
+++ src/day1.ts
@@ -1,25 +1,36 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
 const readInput = (): number[][] => {
-    const contents = fs.readFileSync('inputs/day1Input.txt').toString();
-    
-    const numberSegments = contents.split('\n\n');
+  const contents = fs.readFileSync("inputs/day1Input.txt").toString();
 
-    return numberSegments.map((numberSegment) => numberSegment.split('\n').map((numAsStr) => Number(numAsStr)));
-}
+  const numberSegments = contents.split("\n\n");
+
+  return numberSegments.map((numberSegment) =>
+    numberSegment.split("\n").map((numAsStr) => Number(numAsStr))
+  );
+};
 
 const sum = (nums: number[]) => nums.reduce((total, num) => total + num);
 
 export const answer = (): number => {
-    const caloriesOfFoodItemsCarriedByEachElf = readInput();
+  const caloriesOfFoodItemsCarriedByEachElf = readInput();
 
-    const totalCaloriesCarriedByEachElf = caloriesOfFoodItemsCarriedByEachElf.reduce((acc, cur) => ([...acc, sum(cur)]), []);
+  const totalCaloriesCarriedByEachElf =
+    caloriesOfFoodItemsCarriedByEachElf.reduce(
+      (acc, cur) => [...acc, sum(cur)],
+      []
+    );
 
-    const totalCaloriesCarriedByEachElfSorted = [...totalCaloriesCarriedByEachElf].sort();
+  const totalCaloriesCarriedByEachElfSorted = [
+    ...totalCaloriesCarriedByEachElf,
+  ].sort();
 
-    const top3TotalCaloriesCounts = totalCaloriesCarriedByEachElfSorted.slice(totalCaloriesCarriedByEachElfSorted.length - 3, totalCaloriesCarriedByEachElfSorted.length);
+  const top3TotalCaloriesCounts = totalCaloriesCarriedByEachElfSorted.slice(
+    totalCaloriesCarriedByEachElfSorted.length - 3,
+    totalCaloriesCarriedByEachElfSorted.length
+  );
 
-    const top3TotalCaloriesCountsSummed = sum(top3TotalCaloriesCounts);
+  const top3TotalCaloriesCountsSummed = sum(top3TotalCaloriesCounts);
 
-    return top3TotalCaloriesCountsSummed;
+  return top3TotalCaloriesCountsSummed;
 };
--- src/day2.ts
+++ src/day2.ts
@@ -1,12 +1,12 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
 type Move = "ROCK" | "PAPER" | "SCISSORS";
 
 type Result = "WIN" | "LOSS" | "DRAW";
 
 type Game = {
-    opponentsMove: Move,
-    result: Result
+  opponentsMove: Move;
+  result: Result;
 };
 
 const LOSS_POINTS = 0;
@@ -14,102 +14,106 @@ const DRAW_POINTS = 3;
 const WIN_POINTS = 6;
 
 const readInput = (): Game[] => {
-    const contents = fs.readFileSync('inputs/day2Input.txt').toString().trim();
-    
-    const lines = contents.split('\n');
+  const contents = fs.readFileSync("inputs/day2Input.txt").toString().trim();
 
-    return lines.map((line) => {
-        const [opponentsMove, gameResult] = line.split(' ');
+  const lines = contents.split("\n");
 
-        return {opponentsMove: inputOpponentMoveSymbolToMove(opponentsMove), result: inputGameResult(gameResult)};
-    });
+  return lines.map((line) => {
+    const [opponentsMove, gameResult] = line.split(" ");
+
+    return {
+      opponentsMove: inputOpponentMoveSymbolToMove(opponentsMove),
+      result: inputGameResult(gameResult),
+    };
+  });
 };
 
 const inputOpponentMoveSymbolToMove = (symbol: string): Move => {
-    switch(symbol) {
-        case "A":
-            return "ROCK";
-        case "B":
-            return "PAPER";
-        case "C":
-            return "SCISSORS";
-    }
+  switch (symbol) {
+    case "A":
+      return "ROCK";
+    case "B":
+      return "PAPER";
+    case "C":
+      return "SCISSORS";
+  }
 
-    throw new Error(`Unknown move: ${symbol}`);
+  throw new Error(`Unknown move: ${symbol}`);
 };
 
 const inputGameResult = (symbol: string): Result => {
-    switch(symbol) {
-        case "X":
-            return "LOSS";
-        case "Y":
-            return "DRAW";
-        case "Z":
-            return "WIN";
-    }
+  switch (symbol) {
+    case "X":
+      return "LOSS";
+    case "Y":
+      return "DRAW";
+    case "Z":
+      return "WIN";
+  }
 
-    throw new Error(`Unknown game result: ${symbol}`);
+  throw new Error(`Unknown game result: ${symbol}`);
 };
 
 const sum = (nums: number[]) => nums.reduce((total, num) => total + num);
 
 const scoreForMove = (move: Move): number => {
-    switch(move) {
-        case "ROCK":
-            return 1;
-        case "PAPER":
-            return 2;
-        case "SCISSORS":
-            return 3;
-    }
+  switch (move) {
+    case "ROCK":
+      return 1;
+    case "PAPER":
+      return 2;
+    case "SCISSORS":
+      return 3;
+  }
 };
 
 const scoreForMyMove = (game: Game) => {
-    const myMove = findMyMove(game);
+  const myMove = findMyMove(game);
 
-    return scoreForMove(myMove);
+  return scoreForMove(myMove);
 };
 
 const findMyMove = (game: Game): Move => {
-    if(game.result === "WIN") {
-        if(game.opponentsMove === "PAPER") {
-            return "SCISSORS";
-        } else if (game.opponentsMove === "ROCK") {
-            return "PAPER";
-        }
-
-        return "ROCK";
-    } else if(game.result === "LOSS") {
-        if(game.opponentsMove === "PAPER") {
-            return "ROCK";
-        } else if (game.opponentsMove === "ROCK") {
-            return "SCISSORS";
-        }
-
-        return "PAPER";
+  if (game.result === "WIN") {
+    if (game.opponentsMove === "PAPER") {
+      return "SCISSORS";
+    } else if (game.opponentsMove === "ROCK") {
+      return "PAPER";
     }
 
-    // We picked the same, so draw
-    return game.opponentsMove;
+    return "ROCK";
+  } else if (game.result === "LOSS") {
+    if (game.opponentsMove === "PAPER") {
+      return "ROCK";
+    } else if (game.opponentsMove === "ROCK") {
+      return "SCISSORS";
+    }
+
+    return "PAPER";
+  }
+
+  // We picked the same, so draw
+  return game.opponentsMove;
 };
 
 const scoreForGameResult = (result: Result): number => {
-    switch(result) {
-        case "WIN":
-            return WIN_POINTS;
-        case "DRAW":
-            return DRAW_POINTS;
-        case "LOSS":
-            return LOSS_POINTS;
-    }
+  switch (result) {
+    case "WIN":
+      return WIN_POINTS;
+    case "DRAW":
+      return DRAW_POINTS;
+    case "LOSS":
+      return LOSS_POINTS;
+  }
 };
 
-const calculateScoresForEachGame = (games: Game[]) => games.map((game) => scoreForGameResult(game.result) + scoreForMyMove(game))
+const calculateScoresForEachGame = (games: Game[]) =>
+  games.map((game) => scoreForGameResult(game.result) + scoreForMyMove(game));
 
 export const answer = (): number => {
-    const games = readInput();
+  const games = readInput();
 
-    const scoresForGames = calculateScoresForEachGame(games);
+  const scoresForGames = calculateScoresForEachGame(games);
 
-    return sum(scoresForGames);
+  return sum(scoresForGames);
 };
